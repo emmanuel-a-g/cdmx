408b369d85bef23adcde254a591419927f8486b3
Emmanuel Garcia <earturoga@gmail.com>
2022-07-18 20:13:12 -0500

    added favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,16 @@
 import { ContextProvider } from "../components/context/myContext";
 import Layout from "../components/layout/layout";
 import "../styles/index.css";
+import Head  from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ContextProvider>
       <Layout>
+        <Head>
+          <meta charSet="UTF-8" />
+          <link rel="icon" href="/favico.png" />
+        </Head>
         <Component {...pageProps} />
       </Layout>
     </ContextProvider>
