dc6c6408c630241855b816d63e5911d6099fc620
Emmanuel Garcia <earturoga@gmail.com>
2022-07-20 18:42:04 -0500

    footer and layout updated

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -1,12 +1,12 @@
 import styles from "./footer.module.css";
+import Link from "next/link";
 function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.footerDiv}>
         <p className={styles.story}>
-          About:
-          Fundada en la Ciudad de México en 2010, este año DGE celebra con
-          orgullo 12 años de historia. Durante 1 década hemos creado los más
+          About: Fundada en la Ciudad de México en 2010, este año DGE celebra
+          con orgullo 12 años de historia. Durante 1 década hemos creado los más
           impresionantes objetos contemporáneos de la más alta calidad, así como
           piezas de arte dignas de museo. La mano de obra de DGE es ampliamente
           considerada como insuperable, ya que cada pieza es hecha a mano por
@@ -15,9 +15,27 @@ function Footer() {
         </p>
 
         <p className={styles.story}></p>
-        <p className={styles.story}>Servicio al Cliente</p>
-        <p className={styles.story}>Contacto</p>
-        <p className={styles.story}>Redes Sociales</p>
+        <p className={styles.story}>
+          <Link href="/servicios">
+            <a className={styles.anchor} title="servicio">
+              Servicio al Cliente
+            </a>
+          </Link>
+        </p>
+        <p className={styles.story}>
+          <Link href="/contacto">
+            <a className={styles.anchor} title="contacto">
+              Contacto
+            </a>
+          </Link>
+        </p>
+        <p className={styles.story}>
+          <Link href="/">
+            <a className={styles.anchor} title="redes sociales">
+              Redes Sociales
+            </a>
+          </Link>
+        </p>
       </div>
       <div className={styles.more}>Copyright 2022</div>
     </footer>
--- components/layout/navigation.js
+++ components/layout/navigation.js
@@ -4,27 +4,37 @@ function Navigation() {
   return (
     <nav className={styles.nav}>
       <div className={styles.left}>
-        <img src="./logo.png" width={100} className={styles.logo} />
+        <Link href="/">
+          <img src="./logo.png" width={100} className={styles.logo} />
+        </Link>
       </div>
       <ul className={styles.listRight}>
         <li className={styles.item}>
           <Link href="/">
-            <a className={styles.anchor} title="home">Home</a>
+            <a className={styles.anchor} title="home">
+              Home
+            </a>
           </Link>
         </li>
         <li className={styles.item}>
           <Link href="/productos">
-            <a className={styles.anchor} title="productos">Productos</a>
+            <a className={styles.anchor} title="productos">
+              Productos
+            </a>
           </Link>
         </li>
         <li className={styles.item}>
           <Link href="/servicios">
-            <a className={styles.anchor} title="servicios">Servicios</a>
+            <a className={styles.anchor} title="servicios">
+              Servicios
+            </a>
           </Link>
         </li>
         <li className={styles.item}>
-        <Link href="/contacto">
-            <a className={styles.anchor} title="contacto">Contacto</a>
+          <Link href="/contacto">
+            <a className={styles.anchor} title="contacto">
+              Contacto
+            </a>
           </Link>
         </li>
       </ul>
